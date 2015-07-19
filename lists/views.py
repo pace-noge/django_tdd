c69cfb29a94d09387900d2b630680b0e2c68f87f
nasa <nasa.freaks@gmail.com>
2015-07-20 05:11:08 +0700

    new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -7,10 +7,11 @@ from .models import Item
 def home_page(request):
     if request.method == "POST":
         Item.objects.create(text=request.POST.get('item_text'))
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
     
     items = Item.objects.all()
-    return render(request, 
-        'home.html',
-        {"items": items}
-    )
+    return render(request, 'home.html')
+
+def item_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
