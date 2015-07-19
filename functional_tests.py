da9a75ef2017f88ef269ad0ebe04d9f7ba1d94d4
nasa <nasa.freaks@gmail.com>
2015-07-19 22:17:13 +0700

    functional test second list

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -36,16 +37,24 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         
+        import time
+        # time.sleep(10)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows)
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # there is still a text box inviting to add another item. 
         # Enter "Use peacock feathers to make a fly"
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # the page updates again, and now show both items on her list
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
 
 
         # that site has generated a unique URL for her -- there is some
