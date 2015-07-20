6eca79cfd1fdb6d46f34011115f6dbba8019b9de
nasa <nasa.freaks@gmail.com>
2015-07-20 15:25:53 +0700

    new form for the list items

---
+++ lists/tests/test_forms.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from lists.forms import ItemForm, EMPTY_LIST_ERROR
+
+class ItemFormTest(TestCase):
+    def test_form_renders_item_text_input(self):
+        form = ItemForm(data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            [EMPTY_LIST_ERROR]
+        )
